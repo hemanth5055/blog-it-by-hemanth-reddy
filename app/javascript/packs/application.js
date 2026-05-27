f3d7fb364f3f2a274736ef49d8859f470bd554bd
Kanala Hemanth Reddy <kanalahemanthrdy@gmail.com>
2026-05-27 14:24:48 +0530

    Set up react environment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import "../stylesheets/application.scss"
+import "../stylesheets/application.scss";
+
+import { setAuthHeaders } from "apis/axios";
+
+setAuthHeaders();
