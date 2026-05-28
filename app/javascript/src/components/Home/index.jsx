6ea9bcf6c8cfd988cd762ab0a82e75485ba3be8c
Kanala Hemanth Reddy <kanalahemanthrdy@gmail.com>
2026-05-28 12:04:04 +0530

    Completed setting up react

---
+++ app/javascript/src/components/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Button } from "@bigbinary/neetoui";
+
+const Home = () => (
+  <div>
+    <Button label="This is a button" style="primary" />
+  </div>
+);
+
+export default Home;
